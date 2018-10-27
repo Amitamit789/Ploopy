e6eccb2a9ec5b0a21570a914009a1b338a43fee8
dopeheadz <djdope@naver.com>
2018-10-27 11:26:13 +0900

    django rest framework 설치
    1.pipenv install djangorestframework
    2.config->settings->base.py->서드파이앱->rest_framework 추가
    
    image serializers 추가

--- ploopy/users/admin.py
+++ ploopy/users/admin.py
@@ -12,6 +12,8 @@ class UserAdmin(auth_admin.UserAdmin):
 
     form = UserChangeForm
     add_form = UserCreationForm
-    fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
+    fieldsets = (
+                    ("User Profile", {"fields": ("name","following","followers")}),
+                ) + auth_admin.UserAdmin.fieldsets
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
