92cf62a47ef1e2e9bc8b81a9a19e3f6a5e3a44a0
dopeheadz <djdope@naver.com>
2018-10-29 18:35:13 +0900

    코멘트 API 작성

--- ploopy/images/urls.py
+++ ploopy/images/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
     # re_path(r"(?P<image_id>[0-9]+)/like/", view=views.LikeImage.as_view(), name="like_image"),
     path("<int:image_id>/like/", view=views.LikeImage.as_view(), name="like_image"),
+    path("<int:image_id>/comment/", view=views.CommentOnImage.as_view(), name="comment_image"),
 ]
 
 
