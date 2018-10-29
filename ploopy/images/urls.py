0f1e1d6a5a757593c52df2041e54add99447d5e1
dopeheadz <djdope@naver.com>
2018-10-29 23:42:08 +0900

    팔로잉 API 작성

--- ploopy/images/urls.py
+++ ploopy/images/urls.py
@@ -6,7 +6,8 @@ app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
     # re_path(r"(?P<image_id>[0-9]+)/like/", view=views.LikeImage.as_view(), name="like_image"),
-    path("<int:image_id>/likes/", view=views.LikeImage.as_view(), name="like_image"),
+    path("<int:image_id>/like/", view=views.LikeImage.as_view(), name="like_image"),
+    path("<int:image_id>/unlike/", view=views.UnLikeImage.as_view(), name="like_image"),
     path("<int:image_id>/comments/", view=views.CommentOnImage.as_view(), name="comment_image"),
     path("comments/<int:comment_id>/", view=views.Comment.as_view(), name="comment"),
 ]
--- ploopy/images/views.py
+++ ploopy/images/views.py
@@ -44,10 +44,8 @@ class LikeImage(APIView):
                 creator=user,
                 image=found_image
             )
-            preexisting_like.delete()
-
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
+        
         except models.Like.DoesNotExist:
             new_like = models.Like.objects.create(
                 creator=user,
@@ -57,6 +55,31 @@ class LikeImage(APIView):
             new_like.save()
 
             return Response(status=status.HTTP_201_CREATED)
+        
+        
+class UnLikeImage(APIView):
+
+    def delete(self, request, image_id, format=None):
+        
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            preexisting_like = models.Like.objects.get(
+                creator=user,
+                image=found_image
+            )
+            preexisting_like.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+            
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
+
 
 class CommentOnImage(APIView):
 
@@ -78,8 +101,8 @@ class CommentOnImage(APIView):
 
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
+        
+        
 class Comment(APIView):
     
     def delete(self, request, comment_id, format=None):
@@ -92,5 +115,4 @@ class Comment(APIView):
             return Response(status=status.HTTP_204_NO_CONTENT)
         except models.Comment.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        
         
