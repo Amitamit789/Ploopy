cb71b8c7a98a4d2a8803e076060d3bc81578c92a
dopeheadz <djdope@naver.com>
2018-10-29 22:58:04 +0900

    내가 작성한 댓글 삭제하기

--- ploopy/images/urls.py
+++ ploopy/images/urls.py
@@ -6,8 +6,9 @@ app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
     # re_path(r"(?P<image_id>[0-9]+)/like/", view=views.LikeImage.as_view(), name="like_image"),
-    path("<int:image_id>/like/", view=views.LikeImage.as_view(), name="like_image"),
-    path("<int:image_id>/comment/", view=views.CommentOnImage.as_view(), name="comment_image"),
+    path("<int:image_id>/likes/", view=views.LikeImage.as_view(), name="like_image"),
+    path("<int:image_id>/comments/", view=views.CommentOnImage.as_view(), name="comment_image"),
+    path("comments/<int:comment_id>/", view=views.Comment.as_view(), name="comment"),
 ]
 
 
